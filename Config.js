9cda1b5dbd50b60549e86840423e56521b9a2e51
III12N <147978645+llll12t@users.noreply.github.com>
2025-12-14 03:04:38 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    // API Endpoints
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxPzUUyfvFKBQNhRVMdIVrxLEm6POOoSDuJqQS5p9h5HW3XxcFKvOEUrPD53agPpagK_g/exec',
+
+    // LIFF IDs
+    LIFF_ID: {
+        REGISTER: '2006806033-yY52Xb0x',  // LIFF ID สำหรับหน้าลงทะเบียน
+        PROFILE: '2006806033-LlJE1nO7'     // LIFF ID สำหรับหน้าโปรไฟล์
+    }
+};
