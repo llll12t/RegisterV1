8cd482c28cbbd6c6dc340784bd340a7a3a6a898c
III12N <147978645+llll12t@users.noreply.github.com>
2026-01-16 11:59:47 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 const APP_CONFIG = {
     // API Endpoints
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxPzUUyfvFKBQNhRVMdIVrxLEm6POOoSDuJqQS5p9h5HW3XxcFKvOEUrPD53agPpagK_g/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec',
 
     // LIFF IDs
     LIFF_ID: {
-        REGISTER: '2006806033-yY52Xb0x',  // LIFF ID สำหรับหน้าลงทะเบียน
-        PROFILE: '2006806033-LlJE1nO7'     // LIFF ID สำหรับหน้าโปรไฟล์
+        REGISTER: 'XXXXXXXXXX',  // LIFF ID สำหรับหน้าลงทะเบียน
+        PROFILE: 'XXXXXXXXXXX'     // LIFF ID สำหรับหน้าโปรไฟล์
     }
 };
+
